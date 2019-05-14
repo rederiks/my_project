498275fe160d227bf1f87b9c8fd12af1ad81338f
rederiks <49532028+rederiks@users.noreply.github.com>
2019-05-14 19:46:49 +0300

    cosmetic update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from board import *
 from player_input import *
 
-
-
 def check_field(board):
   field_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,8))
   for field in field_coord:
@@ -30,5 +28,14 @@ def main(board):
         print("Draw!")
         break
   draw_board(board)
+print('Help:\nrules - rules of the game\nstart - start to play\nexit - stop program')
 
-main(board)
+while True:
+
+  choice = input()
+  if choice == 'rules':
+    print('The object of Tic Tac Toe is to get three in a row. You play on a three by three game board. The first player is known as X and the second is O. \nPlayers alternate placing Xs and Os on the game board until either oppent has three in a row or all nine squares are filled. \nX always goes first, and in the event that no one has three in a row, the stalemate is called a cat game.')
+  if choice == 'start':
+    main(board)
+  if choice == 'exit':
+    break
